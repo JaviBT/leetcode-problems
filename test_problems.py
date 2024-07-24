855b08d05634903c5fadee4bc453165923644ffb
JaviBT <francisco.barranco@estudiante.uam.es>
2024-07-24 10:57:31 +0200

    Changes to the test output format

--- test_problems.py
+++ test_problems.py
@@ -6,6 +6,8 @@ import sys
 import time
 import subprocess
 
+ADD_DOTS = True
+
 # Get the current directory
 current_dir = os.getcwd()
 
@@ -39,13 +41,26 @@ problems = []
 for problem in temp:
     problems.append(problem[1])
 
+# Get the legth of the longest problem name in order to format the output
+max_problem_name_length = 0
+for problem in problems:
+    # Get the problem name from the first line of the script
+    problem_path = os.path.join(problems_dir, problem)
+    problem_file = open(problem_path, "r", encoding="utf-8")
+    problem_name = problem_file.readline().strip()
+    problem_file.close()
+    # Remove the #, number and . from the start of the problem name
+    problem_name = problem_name.split(".", 1)[1].strip()
+    max_problem_name_length = max(max_problem_name_length, len(problem_name))
+
 # Print Header:
+LINEPAD = 43
 print("Leetcode Problems by Javi Barranco")
 print("(Only one test case is used for each problem)")
 print("")
-print("-" * 99)
-print("Problem".ljust(14) + "Name".ljust(60) + "Status".ljust(16) + "Time".ljust(5))
-print("-" * 99)
+print("-" * (LINEPAD + max_problem_name_length))
+print("Problem".ljust(14) + "Name".ljust(max_problem_name_length + 4) + "Status".ljust(16) + "Time".ljust(5))
+print("-" * (LINEPAD + max_problem_name_length))
 
 # Run the scripts in the problems directory
 for problem in problems:
@@ -59,7 +74,9 @@ for problem in problems:
     problem_file.close()
     # Remove the #, number and . from the start of the problem name
     problem_name = problem_name.split(".", 1)[1].strip()
-
+    # Add dots to the problem name to align the output
+    if ADD_DOTS:
+        problem_name = problem_name + " " + "." * (max_problem_name_length + 2 - len(problem_name))
 
     # Run the problem (Windows)
     start_time = time.time()
@@ -86,11 +103,11 @@ for problem in problems:
         status_emoji = ("\033[91m" + "✗" + "\033[0m")
 
     # Print the problem number, name, status, score and time taken (Time in miliseconds and 3 decimal places)
-    print("Problem " + "{}".format(problem_number).ljust(4) + ": " + problem_name.ljust(60) + problem_status.ljust(10) + "{}".format(status_emoji) + "     " + str(round(problem_time_taken * 1000, 3)).ljust(6) + " ms")
+    print("Problem " + "{}".format(problem_number).ljust(4) + ": " + problem_name.ljust(max_problem_name_length + 4) + problem_status.ljust(10) + "{}".format(status_emoji) + "     " + str(round(problem_time_taken * 1000, 3)).ljust(6) + " ms")
 
 # Totals
-print("-" * 99)
+print("-" * (LINEPAD + max_problem_name_length))
 print("\n----------- Results ------------")
 print("Overall score".ljust(20) + ": " + str(num_accepted) + "/" + str(num_problems))
-print("Overall time taken".ljust(20) + ": " + str(round(total_time_taken * 1000, 3)).ljust(6) + " ms")
+print("Overall time taken".ljust(20) + ": " + str(round(total_time_taken * 1000, 2)).ljust(6) + " ms")
 print("-" * 32)
